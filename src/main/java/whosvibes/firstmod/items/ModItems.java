b16e9d2258bb8785f5b6eea5a4405787452b7d5f
Christopher <cpw9783@rit.edu>
2024-05-15 18:31:57 -0400

    real initial commit

--- src/main/java/whosvibes/firstmod/items/ModItems.java
+++ src/main/java/whosvibes/firstmod/items/ModItems.java
@@ -1,2 +1,27 @@
-package whosvibes.firstmod.items;public class ModItems {
+package whosvibes.firstmod.items;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import whosvibes.firstmod.Firstmod;
+
+public class ModItems {
+
+    public static final Item BIG_SWORD = registerItem("big_sword", new Item(new Item.Settings()));
+
+    public static void registerModItems(){
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(ModItems::addItemToCreativeTab);
+    }
+
+    public static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(Firstmod.MOD_ID, name), item);
+    }
+
+    public static void addItemToCreativeTab(FabricItemGroupEntries entries){
+        entries.add(BIG_SWORD);
+    }
 }
